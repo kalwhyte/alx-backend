dc79d919db9a3e6b43a095e062d53e7df7275081
kalwhyte <superkalel55@gmail.com>
2023-08-23 19:51:43 +0100

    100-lfu_cache.py

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" 3. LIFO Caching
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ LFUCache defines:
+      - caching system inherit from BaseCaching
+    """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.queue = []
+        self.lfu = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = self.queue.pop(0)
+                del self.cache_data[discard]
+                del self.lfu[discard]
+                print("DISCARD: {}".format(discard))
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if key in self.lfu:
+                self.lfu[key] += 1
+            else:
+                self.lfu[key] = 1
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key and key in self.cache_data:
+            self.lfu[key] += 1
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data[key]
+        return None
