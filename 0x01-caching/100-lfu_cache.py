2d281ea0fc21b9e13d0474263d9317368a4e64b8
kalwhyte <superkalel55@gmail.com>
2023-08-24 17:30:59 +0100

    100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -15,31 +15,35 @@ class LFUCache(BaseCaching):
         """ Initiliaze
         """
         super().__init__()
-        self.queue = []
-        self.freq = {}
+        self.frequency_counter = {}
+        self.used_counter = {}
 
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key and item:
-            if key in self.cache_data:
-                self.queue.remove(key)
-                self.freq[key] += 1
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discard = self.queue.pop(0)
-                del self.cache_data[discard]
-                del self.freq[discard]
-                print("DISCARD: {}".format(discard))
-            self.queue.append(key)
-            self.cache_data[key] = item
-            self.freq[key] = 1
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lfu_keys = [k for k, v in self.frequency_counter.items(
+            ) if v == min(self.frequency_counter.values())]
+            if len(lfu_keys) > 1:
+                lru_key = min(self.used_counter,
+                              key=lambda k: self.used_counter[k])
+                self.cache_data.pop(lru_key)
+                self.frequency_counter.pop(lru_key)
+                self.used_counter.pop(lru_key)
+                print("DISCARD:", lru_key)
+
+        self.cache_data[key] = item
+        self.used_counter[key] = self.used_counter.get(key, 0) + 1
+        self.frequency_counter[key] = self.frequency_counter.get(key, 0) + 1
 
     def get(self, key):
         """ Get an item by key
         """
-        if key and key in self.cache_data:
-            self.queue.remove(key)
-            self.queue.append(key)
-            self.freq[key] += 1
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.used_counter[key] = self.used_counter.get(key, 0) + 1
+        self.frequency_counter[key] = self.frequency_counter.get(key, 0) + 1
+        return self.cache_data[key]
