87947df8934b72287ff407f71c1966ec3e27a575
kalwhyte <superkalel55@gmail.com>
2023-08-24 17:45:59 +0100

    100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -15,35 +15,29 @@ class LFUCache(BaseCaching):
         """ Initiliaze
         """
         super().__init__()
-        self.frequency_counter = {}
-        self.used_counter = {}
+        self.queue = []
+        self.count = {}
 
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is None or item is None:
-            return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            lfu_keys = [k for k, v in self.frequency_counter.items(
-            ) if v == min(self.frequency_counter.values())]
-            if len(lfu_keys) > 1:
-                lru_key = min(self.used_counter,
-                              key=lambda k: self.used_counter[k])
-                self.cache_data.pop(lru_key)
-                self.frequency_counter.pop(lru_key)
-                self.used_counter.pop(lru_key)
-                print("DISCARD:", lru_key)
-
-        self.cache_data[key] = item
-        self.used_counter[key] = self.used_counter.get(key, 0) + 1
-        self.frequency_counter[key] = self.frequency_counter.get(key, 0) + 1
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = self.queue.pop(0)
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+            self.queue.append(key)
+            self.cache_data[key] = item
+            self.count[key] = 0
 
     def get(self, key):
         """ Get an item by key
         """
-        if key is None or key not in self.cache_data:
-            return None
-
-        self.used_counter[key] = self.used_counter.get(key, 0) + 1
-        self.frequency_counter[key] = self.frequency_counter.get(key, 0) + 1
-        return self.cache_data[key]
+        if key and key in self.cache_data:
+            self.count[key] += 1
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data[key]
+        return None
