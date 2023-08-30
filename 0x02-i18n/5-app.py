1daa9174ec5730c9bc50f934b944f14fdfe235a5
kalwhyte <superkalel55@gmail.com>
2023-08-30 04:24:59 +0100

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,13 +4,6 @@ from flask import Flask, render_template, request, g
 from flask_babel import Babel
 from typing import Union, Dict
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 
 class Config:
     """ class attributes equal to en & fr
@@ -24,6 +17,12 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 app.config.from_object(Config)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 @babel.localeselector
